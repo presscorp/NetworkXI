76a91173070dc5f31d6801674f7131bd54531123
Zhalgas Baibatyr <mail@zhalgas.com>
2023-03-18 15:51:48 +0600

    major: Adjusted web-socket to work via Async/await

--- Sources/NetworkXI/WebSocket/WebSocketSessionAdapter.swift
+++ Sources/NetworkXI/WebSocket/WebSocketSessionAdapter.swift
@@ -11,36 +11,21 @@ import Foundation
 /// It's much better to use worker (WebSocketWorker) instead of applying adapter directly for socket message exchange
 public class WebSocketSessionAdapter: SessionAuthChallenger {
 
-    /// Common HTTP headers applied to connection made within this session
     public var additionalHTTPHeaders = [String: String]()
 
     public var defaultSSLChallengeEnabled = false
 
     public var sslCertificates = [NSData]()
 
-    /// Indication of log printing for socket messages into the console
     public var loggingEnabled = false
-
-    public weak var delegate: WebSocketSessionAdapterDelegate?
 
     private weak var session: URLSession?
 
     private let sessionDelegate = SessionDelegationHandler()
 
-    private lazy var receiverCompletionHolder = ReceiverCompletionHolder { [weak self] result in
-        guard let adapter = self, let delegate = adapter.delegate else { return }
+    private var connectContinuation: CheckedContinuation<WebSocketTask, Error>?
 
-        switch result {
-        case .success(let message):
-            switch message {
-            case .data(let data): delegate.received(data: data)
-            case .string(let string): delegate.received(string: string)
-            @unknown default: break
-            }
-        case .failure(let error):
-            adapter.process(error: error)
-        }
-    }
+    private var disconnectContinuation: CheckedContinuation<URLSessionWebSocketTask.CloseCode, Error>?
 
     public required init() {
         sessionDelegate.authChallenge = self
@@ -48,64 +33,46 @@ public class WebSocketSessionAdapter: SessionAuthChallenger {
     }
 
     private func process(error: Error) {
-        if let error = error as NSError?, error.code == NSURLErrorCancelled { return }
-        delegate?.received(error: error)
+        connectContinuation?.resume(throwing: error)
+        disconnectContinuation?.resume(throwing: error)
     }
 }
 
 extension WebSocketSessionAdapter: WebSocketSessionInterface {
 
-    public func resumedTask(with request: URLRequest) -> WebSocketTask {
+    public func resumedTask(with request: URLRequest) async throws -> WebSocketTask {
         let session = URLSession(configuration: .ephemeral, delegate: sessionDelegate, delegateQueue: .main)
-        self.session = session
         let task = session.webSocketTask(with: request)
-        receiverCompletionHolder.setReceiver(for: task)
         task.resume()
         session.finishTasksAndInvalidate()
-        return task
-    }
 
-    public func send(string: String, via task: WebSocketTask) {
-        task.send(.string(string)) { [weak self] error in
-            guard let adapter = self, let delegate = adapter.delegate else { return }
-            if let error = error { return adapter.process(error: error) }
-            delegate.didSend(string: string, result: .success(()))
-        }
-    }
-
-    public func send(data: Data, via task: WebSocketTask) {
-        task.send(.data(data)) { [weak self] error in
-            guard let adapter = self, let delegate = adapter.delegate else { return }
-            if let error = error { return adapter.process(error: error) }
-            delegate.didSend(data: data, result: .success(()))
-        }
-    }
-
-    public func ping(via task: WebSocketTask) {
-        task.sendPing { [weak self] error in
-            guard let adapter = self, let delegate = adapter.delegate else { return }
-            if let error = error { return adapter.process(error: error) }
-            delegate.receivedPong()
-        }
-    }
-}
-
-private extension WebSocketSessionAdapter {
-
-    class ReceiverCompletionHolder {
-
-        private let handle: (_ result: Result<URLSessionWebSocketTask.Message, Error>) -> Void
-
-        init(handler: @escaping (_ result: Result<URLSessionWebSocketTask.Message, Error>) -> Void) {
-            self.handle = handler
+        defer {
+            self.session = session
+            connectContinuation = nil
         }
 
-        func setReceiver(for task: WebSocketTask) {
-            task.receive { [weak self, weak task] result in
-                guard let completionHolder = self, let task = task else { return }
-                completionHolder.handle(result)
-                if case .failure = result { return }
-                completionHolder.setReceiver(for: task)
+        return try await withCheckedThrowingContinuation { [weak self] in self?.connectContinuation = $0 }
+    }
+
+    @discardableResult
+    public func awaitDisconnect() async throws -> URLSessionWebSocketTask.CloseCode {
+        defer { disconnectContinuation = nil }
+        return try await withCheckedThrowingContinuation { [weak self] in self?.disconnectContinuation = $0 }
+    }
+
+    public func send(string: String, via task: WebSocketTask) async throws {
+        try await task.send(.string(string))
+    }
+
+    public func send(data: Data, via task: WebSocketTask) async throws {
+        try await task.send(.data(data))
+    }
+
+    public func ping(via task: WebSocketTask) async throws {
+        return try await withCheckedThrowingContinuation { [weak task] continuation in
+            task?.sendPing { error in
+                if let error { return continuation.resume(throwing: error) }
+                continuation.resume()
             }
         }
     }
@@ -118,7 +85,7 @@ extension WebSocketSessionAdapter: WebSocketLifeCycleService {
         webSocketTask: URLSessionWebSocketTask,
         didOpenWithProtocol protocol: String?
     ) {
-        delegate?.connected()
+        connectContinuation?.resume(returning: webSocketTask)
     }
 
     func urlSession(
@@ -127,8 +94,7 @@ extension WebSocketSessionAdapter: WebSocketLifeCycleService {
         didCloseWith closeCode: URLSessionWebSocketTask.CloseCode,
         reason: Data?
     ) {
-        if loggingEnabled { WebSocketLogger.logDisconnection() }
-        delegate?.disconnected(withCloseCode: closeCode)
+        disconnectContinuation?.resume(returning: closeCode)
     }
 
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
--- Tests/NetworkXITests/Tests/WebSocketTests.swift
+++ Tests/NetworkXITests/Tests/WebSocketTests.swift
@@ -18,116 +18,60 @@ class WebSocketTests: XCTestCase {
         return sessionAdapter
     }()
 
-    lazy var webSocketService: WebSocketService = WebSocketWorker(sessionInterface: sessionInterface)
-    let dispatchGroup = DispatchGroup()
+    lazy var webSocketService: some WebSocketService = WebSocketWorker(sessionInterface: sessionInterface)
 
-    private let string1ToSend = "Test message 1"
-    private let string2ToSend = "Test message 2"
-    private var openConnectionTestClosure: (() -> Void)?
-    private var pingPongClosure: (() -> Void)?
-    private var sendString1TestClosure: (() -> Void)?
-    private var sendString2TestClosure: (() -> Void)?
-    private var receiveString1TestClosure: (() -> Void)?
-    private var receiveString2TestClosure: (() -> Void)?
-    private var closeConnectionTestClosure: (() -> Void)?
-
-    func testWebSocket() {
-
-        webSocketService.set(delegate: self)
+    override func setUp() async throws {
+        try await super.setUp()
         let request = SocketRequest(api_key: "VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
-        webSocketService.connect(using: request)
+        try await webSocketService.connect(using: request)
+    }
 
-        let dispatchGroup = DispatchGroup()
+    override func tearDown() async throws {
+        try await super.tearDown()
+        try await webSocketService.disconnect()
+    }
 
-        dispatchGroup.enter()
-        let expectation0 = expectation(description: "Open connection test")
-        openConnectionTestClosure = {
-            expectation0.fulfill()
-            dispatchGroup.leave()
+    func testPingPong() async throws {
+        let expectation = expectation(description: "Ping-pong")
+
+        do {
+            try await webSocketService.ping()
+            expectation.fulfill()
+        } catch {
+            return XCTAssert(false)
         }
 
-        dispatchGroup.enter()
-        let expectation1 = expectation(description: "Ping-pong test")
-        pingPongClosure = {
-            expectation1.fulfill()
-            dispatchGroup.leave()
-        }
-        webSocketService.ping()
+        wait(for: [expectation], timeout: 3)
+    }
 
-        dispatchGroup.enter()
-        let expectation2 = expectation(description: "Send string 1 test")
-        sendString1TestClosure = {
-            expectation2.fulfill()
-            dispatchGroup.leave()
-        }
-        webSocketService.send(string: string1ToSend)
+    func testSendReceiveMessage() async {
+        let stringMessage = UUID().uuidString
 
-        dispatchGroup.enter()
-        let expectation3 = expectation(description: "Send string 2 test")
-        sendString2TestClosure = {
-            expectation3.fulfill()
-            dispatchGroup.leave()
-        }
-        webSocketService.send(string: string2ToSend)
-
-        dispatchGroup.enter()
-        let expectation4 = expectation(description: "Receive string 1 test")
-        receiveString1TestClosure = {
-            expectation4.fulfill()
-            dispatchGroup.leave()
+        let expectation1 = expectation(description: "Send message")
+        Task {
+            do {
+                try await webSocketService.send(string: stringMessage)
+                expectation1.fulfill()
+            } catch {
+                return XCTAssert(false)
+            }
         }
 
-        dispatchGroup.enter()
-        let expectation5 = expectation(description: "Receive string 2 test")
-        receiveString2TestClosure = {
-            expectation5.fulfill()
-            dispatchGroup.leave()
+        let expectation2 = expectation(description: "Receive message")
+        Task {
+            do {
+                for try await message in webSocketService {
+                    guard case .string(let string) = message else { continue }
+                    if string == stringMessage {
+                        expectation2.fulfill()
+                    }
+                }
+            } catch {
+                return XCTAssert(false)
+            }
         }
 
-        let expectation6 = expectation(description: "Close connection test")
-        closeConnectionTestClosure = {
-            expectation6.fulfill()
-        }
-
-        dispatchGroup.notify(queue: .main) { [weak webSocketService] in
-            webSocketService?.disconnect()
-        }
-
-        let expectations = [
-            expectation0, expectation1, expectation2, expectation3, expectation4, expectation5, expectation6
-        ]
+        let expectations = [expectation1, expectation2]
         wait(for: expectations, timeout: 3)
-    }
-}
-
-extension WebSocketTests: WebSocketDelegate {
-
-    func connected() {
-        openConnectionTestClosure?()
-    }
-
-    func disconnected() {
-        closeConnectionTestClosure?()
-    }
-
-    func didSend(string: String, result: Result<Void, Error>) {
-        guard case .success = result else { return }
-        if string == string1ToSend {
-            sendString1TestClosure?()
-        } else if string == string2ToSend {
-            sendString2TestClosure?()
-        }
-    }
-
-    func received(string: String) {
-        if string == string1ToSend {
-            receiveString1TestClosure?()
-        } else if string == string2ToSend {
-            receiveString2TestClosure?()
-        }
-    }
-
-    func receivedPong() {
-        pingPongClosure?()
     }
 }
